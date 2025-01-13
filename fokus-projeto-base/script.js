88776da3557e0f02c13d53a64b1f758c5308e298
devlarisarapio <larissa.sarapio@gmail.com>
2025-01-12 23:39:19 -0300

    feat: mudança de imagem ao clicar no botao

--- fokus-projeto-base/script.js
+++ fokus-projeto-base/script.js
@@ -2,9 +2,15 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+
+function alterarContexto() {
+    html.setAttribute('data-contexto', contexto)
+}
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', './imagens/foco.png')
 })
 
 curtoBt.addEventListener('click', () => {
